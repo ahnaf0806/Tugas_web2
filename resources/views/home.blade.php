f320ec7e9daf28e703d7f0b602d6696064cb31d2
cosmixi <denta6480@gmail.com>
2025-10-24 10:49:47 +0700

    home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>DaharGo</title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+    <header class="hero">
+        <div class="hero-content">
+            <h1 class="company-name">DaharGo</h1>
+            <p class="tagline">Makan Zaman Now, Pakai DaharGo!</p>
+        </div>
+    </header>
+</body>
+
+</html>
