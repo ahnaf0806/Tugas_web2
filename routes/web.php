55c3f2c0468007867991e50c4777fa8a67aefa4c
ahnaf0806 <ahnafogy81@gmail.com>
2025-10-24 20:20:44 +0700

    FIX

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-use Illuminate\app\Http\Controllers\PerusahaanController;
+use App\Http\Controllers\PerusahaanController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('home', PerusahaanController::class, 'Home');
+Route::get('home', [PerusahaanController::class, 'Home']);
