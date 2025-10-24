fbe59242c5ad9c83c0a34634deef24159b213346
ahnaf0806 <ahnafogy81@gmail.com>
2025-10-24 20:01:24 +0700

    Route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use Illuminate\app\Http\Controllers\PerusahaanController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('home', PerusahaanController::class, 'Home');
