d4537a73378c3eaba221a72185f69c43d97fb392
PStars03 <petrixyoga@gmail.com>
2025-10-25 18:04:30 +0700

    update bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,9 +6,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>DaharGo</title>
     <link rel="stylesheet" href="style.css" />
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 
 <body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" href="#">Features</a>
+                    <a class="nav-link" href="#">Pricing</a>
+                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                </div>
+            </div>
+        </div>
+    </nav>
     <header class="hero">
         <div class="hero-content">
             <h1 class="company-name">DaharGo</h1>
