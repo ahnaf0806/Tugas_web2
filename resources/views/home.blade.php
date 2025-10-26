a766b32f5d8c2124a940dc0ec7bcb2cad76a5414
ahnaf0806 <ahnafogy81@gmail.com>
2025-10-26 22:23:02 +0700

    home done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,9 +9,11 @@
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 
-<body class="bg-dark">
-    <nav class="navbar navbar-expand-lg bg-dark navbar-dark shadow-sm fixed-top navbar-custom " id="mainNav">
+<body class="bg-dark d-flex flex-column min-vh-100 bg-dark text-light">
+    <nav class="navbar navbar-expand-lg bg-dark navbar-dark shadow-sm fixed-top navbar-custom border border-secondary flex-grow-1"
+        id="mainNav">
         <div class="container">
+            <img src="{{ asset('img/Logo.png')}}" class="logo">
             <h1 class="navbar-brand fs-3 text-info" href="#">DaharGo</h1>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
                 aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -26,16 +28,16 @@
             </div>
         </div>
     </nav>
-    <div class="container-fluid banner">
+    <div class="container-fluid banner text-light text-center">
         <div class="container banner-konten">
-            <h1 class="company-name fs-1">DaharGo</h1>
-            <p class="tagline fs-1">Makan Zaman Now, Pakai DaharGo!</p>
         </div>
+        <h1 class="company-name h1">DaharGo</h1>
+        <p class="tagline fs-4">Makan Zaman Now, Pakai DaharGo!</p>
     </div>
-    <div class="container pt-3 text-center k text-light">
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima iste laudantium asperiores porro.
-            Sit eaque minima consectetur, veniam nostrum deleniti?</p>
-    </div>
+
+    <footer class="bg-dark text-center text-light py-3 mt-auto border-top border-secondary">
+        <p class="mb-0">&copy; 2025 DaharGo. All rights reserved.</p>
+    </footer>
     <script src="{{ asset('js/index.js') }}"></script>
 </body>
 
